cf0af0ddf158c6ef9cff9dd866e76cdfdbde9907
Isaac Farias <isaacfarias006@gmail.com>
2023-01-11 10:40:52 -0300

    Adiciona classe para o request

---
+++ ResidenciaDesafio2/ConversaoReq.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio2
+{
+    public class ConversaoReq
+    {
+        public ConversaoReq(string moedaOrigem, string moedaDestino, double valorMonetario)
+        {
+            MoedaOrigem = moedaOrigem;
+            MoedaDestino = moedaDestino;
+            ValorMonetario = valorMonetario;
+        }
+
+        public string MoedaOrigem { get; private set; }
+        public string MoedaDestino { get; private set; }
+        public double ValorMonetario { get; private set; }
+    }
+}
