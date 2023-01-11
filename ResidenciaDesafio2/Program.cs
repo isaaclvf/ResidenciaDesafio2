bdc8663110e2e8240259b19fb1cb1ade737fba07
Isaac Farias <isaacfarias006@gmail.com>
2023-01-11 10:41:08 -0300

    Teste de métodos

--- ResidenciaDesafio2/Program.cs
+++ ResidenciaDesafio2/Program.cs
@@ -1,7 +1,18 @@
-﻿using ResidenciaDesafio2.Service;
+﻿using ResidenciaDesafio2;
+using ResidenciaDesafio2.Service;
 
-//// Teste
-//using HttpClient client = new();
+using HttpClient client = new();
+var moedas = await Conversor.GetMoedasValidas(client);
+
+// Testes
+
+//foreach (var m in moedas)
+//{
+//    Console.WriteLine(m);
+//}
+
 //var conversor = new Conversor(client);
 //var resultado = await conversor.Converter("BRL", "USD", 100);
 //Console.WriteLine(resultado);
+
+Controller.Start(moedas);
